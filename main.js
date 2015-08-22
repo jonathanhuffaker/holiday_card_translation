40685183f84a75017ce5eb8c07195af784210364
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-08-22 11:22:26 -0500

    Changes to JS file

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 // var test = alert("test");
 // console.log(test);
 //above is just a test to make sure we had linked our file correctly
+
+var holidayGreeting = {
+	langDefault = "english";
+	langSpan = "spanish";
+	langGerm = "german";
+}
+
+
+
+var transToSpanish = {
+"merry":"alegre", "christmas":"Navidad", "and":"y", "happy":"feliz", "new":"nuevo", "year":"ano"
+}
+
